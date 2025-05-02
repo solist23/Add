b437873d8faffa75274770101a3581845d76a413
solist23 <snowman.bs2005@gmail.com>
2025-05-02 12:44:09 +0300

    Add godoc comments for package and Add function

--- add.go
+++ add.go
@@ -1,5 +1,12 @@
+// Package mathutils provides basic mathematical utilities.
+//
+// This package includes simple arithmetic operations like addition.
 package Add
 
+// Add takes two integers and returns their sum.
+//
+// For more information about addition, visit:
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a1, a2 int) int {
 	return a1 + a2
 
