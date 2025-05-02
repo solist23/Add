303e628d99e900da1838e1de576cc961882cca1d
solist23 <snowman.bs2005@gmail.com>
2025-05-02 13:04:20 +0300

    Add go.mod; make Add generic

--- add.go
+++ add.go
@@ -3,11 +3,19 @@
 // This package includes simple arithmetic operations like addition.
 package Add
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add takes two integers and returns their sum.
 //
 // For more information about addition, visit:
 // https://www.mathsisfun.com/numbers/addition.html
-func Add(a1, a2 int) int {
+func Add[T Number](a1, a2 T) T {
 	return a1 + a2
 
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/solist23/Add
+
+go 1.24.2
+
+require golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0
