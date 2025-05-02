516fbbcabd361481dd7dac3013d3c30c3f355086
solist23 <snowman.bs2005@gmail.com>
2025-05-02 14:21:44 +0300

    Add govulncheck

--- add.go
+++ add.go
@@ -7,15 +7,16 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// Number is a type constraint that permits any integer or floating-point type.
 type Number interface {
 	constraints.Integer | constraints.Float
 }
 
-// Add takes two integers and returns their sum.
+// Add takes two numbers of type Number and returns their sum.
 //
+// Supported types include all integers and floating-point numbers.
 // For more information about addition, visit:
 // https://www.mathsisfun.com/numbers/addition.html
-func Add[T Number](a1, a2 T) T {
-	return a1 + a2
-
+func Add[T Number](a, b T) T {
+	return a + b
 }
